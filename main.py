5fe1196b98bb5f83b74f442ad241dd1e4900b554
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-05-08 19:43:25 +0200

    first working checkpoint

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 
-if __name__ == "__main__":
-    print()
+import sys
+from lib.ping import check_status
+
+
+ip_addr = sys.argv[1:]
+
+if len(ip_addr) == 0:
+    print('no ip address where passed')
+    exit(0)
+
+# update all ip statuses
+for i in range(len(ip_addr)):
+    ip_status = check_status(ip_addr[i])
+    if ip_status is None:
+        print(ip_addr[i] + ': is on')
+    else:
+        print(ip_addr[i] + ': ' + ip_status)
--- test.py
+++
@@ -1,5 +0,0 @@
-#!/bin/env python3
-
-from lib.ping import check_status
-
-print(check_status('10.201.10.10'))
